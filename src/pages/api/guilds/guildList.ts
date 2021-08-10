0fc6b99636953ea3b2cffd470f1241122b6f0690
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2021-08-10 21:29:35 +0200

    Added serverless-mysql package for mysql queries

--- src/pages/api/guilds/guildList.ts
+++ src/pages/api/guilds/guildList.ts
@@ -1,35 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import mysql from "mysql";
-
 import { Guild } from "../../../common/types";
 
-const con = mysql.createConnection(process.env.DATABASE_URL + "");
+const mysql = require("serverless-mysql")({
+  config: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+});
 
 const guildHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const userId = req.query.userId;
-
-  con.query(
-    `SELECT access_token FROM accounts WHERE user_id = ${userId}`,
-    async (error, results) => {
-      if (error) throw error;
-      if (!results.length) {
-        res.status(500).json({ error: "No account found" });
-        return;
-      }
-
-      const guildRes = await axios.get(
-        "https://discord.com/api/users/@me/guilds",
-        { headers: { Authorization: `Bearer ${results[0].access_token}` } }
-      );
-
-      const filteredGuilds = guildRes.data.filter((guild: Guild) => {
-        return guild.permissions === 2147483647;
-      });
-
-      res.status(200).json(filteredGuilds);
-    }
+  const results = await mysql.query(
+    `SELECT access_token FROM accounts WHERE user_id = ${userId}`
   );
+  if (!results.length) {
+    res.status(500).json({ error: "No account found" });
+    return;
+  }
+
+  const guildRes = await axios.get("https://discord.com/api/users/@me/guilds", {
+    headers: { Authorization: `Bearer ${results[0].access_token}` },
+  });
+  const filteredGuilds = guildRes.data.filter((guild: Guild) => {
+    return guild.permissions === 2147483647;
+  });
+
+  await mysql.end();
+  res.status(200).json(filteredGuilds);
 };
 
 export default guildHandler;
