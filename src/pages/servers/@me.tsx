a4295afd2fbf9c09e325db64538cf210b734d33e
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2021-08-12 17:18:14 +0200

    Added swr to data fetching for better TTFB

--- src/pages/servers/@me.tsx
+++ src/pages/servers/@me.tsx
@@ -1,12 +1,14 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect } from "react";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import useSWR from "swr";
 
 import axios from "axios";
 
 import { Guild } from "../../common/types";
-import { getSession } from "next-auth/client";
+import { getSession, useSession } from "next-auth/client";
 
 import serverStyles from "../../styles/MyServer.module.scss";
 
@@ -14,41 +16,68 @@ type Props = {
   guildList: Array<Guild>;
 };
 
-const MyServers: FunctionComponent<Props> = ({ guildList }) => {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const MyServers: FunctionComponent<Props> = () => {
+  const [session] = useSession();
+  const {
+    data: guildList,
+    error,
+    isValidating,
+  } = useSWR(() => "/api/guilds/guildList?userId=" + session?.id, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnMount: false,
+    shouldRetryOnError: false,
+  });
+
   return (
-    <section className={serverStyles.container}>
-      <h1>Select your server</h1>
-      {guildList && (
-        <div className={serverStyles.serverList}>
-          {" "}
-          {guildList?.map((guildItem: Guild) => (
-            <Link key={guildItem.id} href={`/servers/${guildItem.id}`} passHref>
-              <div className={serverStyles.serverItem}>
-                {" "}
-                <div className={serverStyles.serverInfo}>
-                  {guildItem.icon ? (
-                    <Image
-                      src={`https://cdn.discordapp.com/icons/${guildItem.id}/${guildItem?.icon}.png`}
-                      alt="guild icon"
-                      width={48}
-                      height={48}
-                    />
+    <>
+      <Head>
+        <title>Your Servers</title>
+      </Head>
+      <section className={serverStyles.container}>
+        <h1>Select your server</h1>
+        {isValidating && (
+          <div>
+            <h1>Loading...</h1>
+          </div>
+        )}
+        {guildList && !error && (
+          <div className={serverStyles.serverList}>
+            {" "}
+            {guildList?.map((guildItem: Guild) => (
+              <Link
+                key={guildItem.id}
+                href={`/servers/${guildItem.id}`}
+                passHref
+              >
+                <div className={serverStyles.serverItem}>
+                  {" "}
+                  <div className={serverStyles.serverInfo}>
+                    {guildItem.icon ? (
+                      <Image
+                        src={`https://cdn.discordapp.com/icons/${guildItem.id}/${guildItem?.icon}.png`}
+                        alt="guild icon"
+                        width={48}
+                        height={48}
+                      />
+                    ) : (
+                      <span>{guildItem.name.match(/\b(\w)/g)}</span>
+                    )}
+                    <h2>{guildItem.name}</h2>
+                  </div>
+                  {guildItem?.id ? (
+                    <button className="btn-primary">Dashboard</button>
                   ) : (
-                    <span>{guildItem.name.match(/\b(\w)/g)}</span>
+                    <button>Set Up</button>
                   )}
-                  <h2>{guildItem.name}</h2>
                 </div>
-                {guildItem?.id ? (
-                  <button className="btn-primary">Dashboard</button>
-                ) : (
-                  <button>Set Up</button>
-                )}
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-    </section>
+              </Link>
+            ))}
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
