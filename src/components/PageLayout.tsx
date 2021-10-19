4aa4ef345f338ee00568822ec55079f3d4be6966
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2021-10-19 20:44:18 +0200

    Added temp 404 voxel image Updated dependencies

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -12,7 +12,7 @@ const PageLayout = ({ children }: LayoutProps) => {
       <header className="max-w-6xl mx-auto">
         <MainNav />
       </header>
-      <main className="max-w-6xl min-h-[48em] mx-auto">{children}</main>
+      <main className="max-w-6xl min-h-[40em] mx-auto">{children}</main>
       <footer className="max-w-6xl mx-auto flex flex-col sm:flex-row justify-between px-4 sm:px-8 my-10 mt-32">
         <FooterNav />
       </footer>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 import notFound from "../styles/Custom404.module.scss";
 
 const Custom404 = () => {
@@ -8,10 +9,18 @@ const Custom404 = () => {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div className="h-[80vh] flex flex-col text-center items-center justify-center px-4 md:px-8">
-        <h1 className="text-5xl my-2 px-6 pb-2 border-b border-gray-700">
+      <div className="h-[60vh] flex flex-col text-center items-center justify-center px-4 md:px-8">
+        <div className="relative w-2/3 h-80 mb-8">
+          <Image
+            src="/img/voxel/NotFoundVoxel.png"
+            alt="NotFound"
+            layout="fill"
+            className="object-contain"
+          ></Image>
+        </div>
+        {/* <h1 className="text-5xl my-2 px-6 pb-2 border-b border-gray-700">
           404
-        </h1>
+        </h1> */}
         <h1 className="mb-4 text-md md:text-xl">
           The page you were looking for could not be found
         </h1>
