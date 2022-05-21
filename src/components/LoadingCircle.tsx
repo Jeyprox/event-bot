80973e6c7e71030b687295958fb5257d3753ea9c
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2022-05-22 00:20:51 +0200

    fixed loading circle position

--- src/components/LoadingCircle.tsx
+++ src/components/LoadingCircle.tsx
@@ -1,6 +1,6 @@
 const LoadingCircle = () => {
   return (
-    <div className="animate-spin w-12 h-12 rounded-full border-8 border-gray-800 border-t-gray-700"></div>
+    <div className="mx-auto my-8 animate-spin w-12 h-12 rounded-full border-8 border-gray-800 border-t-gray-700"></div>
   );
 };
 
